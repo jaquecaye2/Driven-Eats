04f91949e7947ae83d27f4f07ccab5d515fcb60e
jaquecaye <jaquelinecaye@gmail.com>
2022-04-02 19:38:48 -0300

    Enviar mensagem no whatsapp

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function selecionarPrato(prato){
+    // Adicionar a borda
     let pratoSelecionado = document.querySelector(".prato-selecionado");
 
     if (pratoSelecionado !== null) {
@@ -7,7 +8,16 @@ function selecionarPrato(prato){
 
     prato.classList.add("prato-selecionado")
 
-    console.log(prato)
+    // Adicionar o checkbox
+    let checkAtivado = document.querySelector(".icone-prato")
+
+    if (checkAtivado !== null) {
+        checkAtivado.classList.remove("icone-prato")
+    }
+
+    prato.querySelector("ion-icon").classList.add("icone-prato")
+
+    mudarBotaoFinalizar()
 }
 
 function selecionarBebida(bebida){
@@ -18,6 +28,16 @@ function selecionarBebida(bebida){
     }
 
     bebida.classList.add("bebida-selecionada")
+
+    let checkAtivado = document.querySelector(".icone-bebida")
+
+    if (checkAtivado !== null) {
+        checkAtivado.classList.remove("icone-bebida")
+    }
+
+    bebida.querySelector("ion-icon").classList.add("icone-bebida")
+
+    mudarBotaoFinalizar()
 }
 
 function selecionarSobremesa(sobremesa){
@@ -28,9 +48,19 @@ function selecionarSobremesa(sobremesa){
     }
 
     sobremesa.classList.add("sobremesa-selecionada")
+
+    let checkAtivado = document.querySelector(".icone-sobremesa")
+
+    if (checkAtivado !== null) {
+        checkAtivado.classList.remove("icone-sobremesa")
+    }
+
+    sobremesa.querySelector("ion-icon").classList.add("icone-sobremesa")
+
+    mudarBotaoFinalizar()
 }
 
-function finalizarPedido(){
+function mudarBotaoFinalizar(){
     let prato = document.querySelector(".prato-selecionado")
     let bebida = document.querySelector(".bebida-selecionada");
     let sobremesa = document.querySelector(".sobremesa-selecionada")
@@ -38,6 +68,34 @@ function finalizarPedido(){
     if (prato !== null && bebida !== null && sobremesa !== null) {
         document.querySelector("button").classList.add("botao-habilitado")
         document.querySelector("button").innerHTML = "Fechar pedido"
+    }
+}
+
+function finalizarPedido(){
+    let botaoHabilitado = document.querySelector(".botao-habilitado")
+
+    let prato = document.querySelector(".prato-selecionado").querySelector("h3").innerHTML
+    let valorPrato = document.querySelector(".prato-selecionado").querySelector("h4").innerHTML
+    let valorPratoNumber = Number(valorPrato.slice("3","8").replace(",","."))
+
+    let bebida = document.querySelector(".bebida-selecionada").querySelector("h3").innerHTML
+    let valorBebida = document.querySelector(".bebida-selecionada").querySelector("h4").innerHTML
+    let valorBebidaNumber = Number(valorBebida.slice("3","8").replace(",","."))
+
+
+    let sobremesa = document.querySelector(".sobremesa-selecionada").querySelector("h3").innerHTML
+    let valorSobremesa = document.querySelector(".sobremesa-selecionada").querySelector("h4").innerHTML
+    let valorSobremesaNumber = Number(valorSobremesa.slice("3","8").replace(",","."))
+    
+    let somaValores = valorPratoNumber + valorBebidaNumber + valorSobremesaNumber
+
+    if (botaoHabilitado !== null){
+        let celular = "5555997048940"
+
+        let mensagem = `Olá, gostaria de fazer o *pedido*: \n *- Prato*: ${prato} \n *- Bebida*: ${bebida} \n *- Sobremesa*: ${sobremesa} \n *Total*: R$*${somaValores.toFixed(2)}*`
+
+        mensagem = window.encodeURIComponent(mensagem)
+        window.open(`https://wa.me/${celular}?text=${mensagem}`)
     }
 }
 
